4add101795916eb069d684595782103a5d151e6f
samgdf <sam@rutgers.io>
2017-05-19 17:11:08 -0400

    Voltair Updates
    Please don’t use this I haven’t changed the API key

--- Voltair/Voltair/ViewController.swift
+++ Voltair/Voltair/ViewController.swift
@@ -11,8 +11,8 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var temperature: UILabel!
     
     var locationManager:CLLocationManager?
     var currentLocation:CLLocation?
@@ -37,24 +37,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    @IBAction func userLocation(_ sender: Any) {
-        let center = currentLocation?.coordinate
-        let region = MKCoordinateRegion(center: center!, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
-        self.mapView.setRegion(region, animated: true)
-        
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(currentLocation!) { (placemarks, error) in
-            if (error == nil) {
-                if let addrList = placemarks?[0].addressDictionary?["FormattedAddressLines"] as? [String] {
-                    let originAddress = addrList.joined(separator: ", ")
-                    self.address.text = originAddress
-                }
-            }
-            
-        }
-    }
-    
     func enableLocation() {
         locationManager?.requestWhenInUseAuthorization()
         
@@ -68,4 +50,5 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             currentLocation = location
         }
     }
+
 }
